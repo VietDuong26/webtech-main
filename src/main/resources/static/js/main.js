17248642883edcb712b6c6fcae6a799d51d4869d
Ryiong <viethung1209.work@gmail.com>
2024-10-23 14:56:52 +0700

    Fix header login and re css

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,17 @@
 //Scale Web
 const elementChange = document.querySelector('body');
-const offerLayout = document.querySelector('.offers-layout')
 
 function setScale() {
     if (window.innerWidth < 1280) {
         let zoom = window.innerWidth / 1280;
         elementChange.style.zoom = zoom;
     }
-    if (window.innerWidth < offerLayout.clientWidth) {
-        let zoom = window.innerWidth / 1360;
-        offerLayout.style.zoom = zoom;
+    if (location.pathname == '/index') {
+        const offerLayout = document.querySelector('.offers-layout')
+        if (window.innerWidth < offerLayout.clientWidth) {
+            let zoom = window.innerWidth / 1360;
+            offerLayout.style.zoom = zoom;
+        }
     }
 }
 setScale();
@@ -17,11 +19,13 @@ document.addEventListener("DOMContentLoaded", setScale);
 window.addEventListener("resize", setScale);
 window.addEventListener("load", setScale);
 
-const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        speed: 1000,
-    },
-})
+if (location.pathname == '/index') {
+    const swiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            speed: 1000,
+        },
+    })
+}
