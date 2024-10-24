0da01256b9bedfe1bff54c1ef117c9e1372b1ad4
Ryiong <viethung1209.work@gmail.com>
2024-10-24 14:47:10 +0700

    Compelete detail product and other file

--- src/main/resources/static/js/productdetail.js
+++ src/main/resources/static/js/productdetail.js
@@ -11,3 +11,48 @@ for (let displayProduct of displayProducts) {
         displayProduct.classList.add('open-product')
     })
 }
+
+const listSize = document.querySelectorAll(".select-size button");
+const listColor = document.querySelectorAll(".select-color button");
+
+for (let sizeItem of listSize) {
+    sizeItem.addEventListener('click', () => {
+        if (sizeItem.classList.contains('select-button')) {
+            sizeItem.classList.remove('select-button');
+            removeValueSelect(sizeItem);
+        } else {
+            removeSelect(listSize, 'select-button');
+            sizeItem.classList.add('select-button');
+            getValueSelect(sizeItem);
+        }
+    })
+}
+
+for (let colorItem of listColor) {
+    colorItem.addEventListener('click', () => {
+        if (colorItem.classList.contains('select-button')) {
+            colorItem.classList.remove('select-button');
+            removeValueSelect(colorItem);
+        } else {
+            removeSelect(listColor, 'select-button');
+            colorItem.classList.add('select-button');
+            getValueSelect(colorItem);
+        }
+    })
+}
+
+function removeSelect(list, nameClass) {
+    for (let i = 0; i < list.length; i++) {
+        list[i].classList.remove(nameClass)
+    }
+}
+
+function getValueSelect(element) {
+    element.parentElement.setAttribute("value", element.getAttribute("value"));
+}
+
+
+function removeValueSelect(element) {
+    element.parentElement.setAttribute("value", "");
+}
+
