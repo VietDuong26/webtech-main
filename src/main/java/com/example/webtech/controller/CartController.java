086fa2de16e162415fab72b71f8396229f51623f
unknown <duong26122003dddppp@gmail.com>
2024-11-02 18:51:46 +0700

    2/11/2024: fix cart after checking out

--- src/main/java/com/example/webtech/controller/CartController.java
+++ src/main/java/com/example/webtech/controller/CartController.java
@@ -63,8 +63,8 @@ public class CartController {
         return "redirect:/cart";
     }
     @GetMapping("/checkout")
-    String checkout(){
+    ResponseEntity<Boolean> checkout(){
         ordersService.checkOut();
-        return "redirect:/cart";
+        return ResponseEntity.status(HttpStatus.OK).body(true);
     }
 }
