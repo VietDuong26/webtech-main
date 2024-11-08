83bbbb6000c15aabfd3301bb0674f026888a5dae
unknown <duong26122003dddppp@gmail.com>
2024-11-08 21:17:06 +0700

    8-11-2024

--- src/main/java/com/example/webtech/controller/SizeController.java
+++ src/main/java/com/example/webtech/controller/SizeController.java
@@ -11,37 +11,45 @@ import org.springframework.web.bind.annotation.*;
 public class SizeController {
     @Autowired
     SizeService sizeService;
+
     @GetMapping("/getAllSize")
-    String getAllSize(Model model){
-        model.addAttribute("sizes",sizeService.getAll());
+    String getAllSize(Model model) {
+        model.addAttribute("sizes", sizeService.getAll());
         return "admin/admin-size_list";
     }
+
     @GetMapping("/addNewSize")
-    String show_addNewSize(Model model){
-        model.addAttribute("new_size",new Size());
+    String show_addNewSize(Model model) {
+        model.addAttribute("new_size", new Size());
         return "admin/admin-size_add";
     }
+
     @PostMapping("/addNewSize")
     String addNewColor(Model model
-            ,@ModelAttribute("new_size")Size new_size
-    ){
-        if(sizeService.checkIfExist(new_size.getSizeName())==false){
+            , @ModelAttribute("new_size") Size new_size
+    ) {
+        if (sizeService.checkIfExist(new_size.getSizeName()) == false && new_size.getSizeName() != "") {
             sizeService.saveOrUpdate(new_size);
         }
         return "redirect:/getAllSize";
     }
+
     @GetMapping("/updateSize/{id}")
-    String show_updateSize(Model model,@PathVariable("id")long id){
-        model.addAttribute("selected_size",sizeService.findById(id));
+    String show_updateSize(Model model, @PathVariable("id") long id) {
+        model.addAttribute("selected_size", sizeService.findById(id));
         return "admin/admin-size_update";
     }
+
     @PostMapping("/updateSize")
-    String updateSize(@ModelAttribute("selected_size")Size size){
-        sizeService.saveOrUpdate(size);
+    String updateSize(@ModelAttribute("selected_size") Size size) {
+        if (!size.getSizeName().equals("") && !sizeService.checkIfExist(size.getSizeName())) {
+            sizeService.saveOrUpdate(size);
+        }
         return "redirect:/getAllSize";
     }
+
     @RequestMapping("/deleteSize/{id}")
-    String deleteSize(@PathVariable("id")long id){
+    String deleteSize(@PathVariable("id") long id) {
         sizeService.delete(id);
         return "redirect:/getAllSize";
     }
